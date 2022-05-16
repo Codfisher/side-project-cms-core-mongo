db50b132bc91341271adbd74116f7690cc23d1a1
Codfisher <pony2140@livemail.tw>
2022-05-17 02:30:12 +0800

    feat: 新增 JwtPayload 型別定義

--- src/auth/auth.type.ts
+++ src/auth/auth.type.ts
@@ -4,7 +4,9 @@ export interface VerifyFirebaseIdTokenError {
   info?: any;
 }
 
-/** 被 passport 嵌入 request 中的 user 物件 */
-export interface RequestUser {
+export interface JwtPayload {
   id: string;
 }
+
+/** 被 passport 嵌入 request 中的 user 物件 */
+export type RequestUser = JwtPayload;
